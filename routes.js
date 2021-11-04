349567e3a4c182ced2368b22a997d8a51426480e
Luan Xavier <luantenorio3@gmail.com>
2021-11-04 12:05:17 -0300

    feat: add instructors route

--- routes.js
+++ routes.js
@@ -2,7 +2,15 @@ const express = require('express');
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
-  res.send('Okay');
-})
+  return res.redirect('/instructors');
+});
+
+routes.get('/instructors', (req, res) => {
+  return res.render('instructors/index');
+});
+
+routes.get('/members', (req, res) => {
+  return res.send('/members');
+});
 
 module.exports = routes;
